d0efaa0a18e1b9fe904cc5592bf83b1ba5cf6d7d
juanCC123 <juanc_villarealo@soy.sena.edu.co>
2024-08-31 15:17:13 -0500

    modificado l

--- src/app/pages/portfolio/page.js
+++ src/app/pages/portfolio/page.js
@@ -9,7 +9,7 @@ export default function portfolio() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
+      <div className="container mt-12 mx-auto px-6 py-2">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
