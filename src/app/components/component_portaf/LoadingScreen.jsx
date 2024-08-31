94e62eed07b8cdaedf28022d34c9e8cebde36ea4
juanCC123 <juanc_villarealo@soy.sena.edu.co>
2024-08-31 12:40:20 -0500

    modificado w

--- src/app/components/component_portaf/LoadingScreen.jsx
+++ src/app/components/component_portaf/LoadingScreen.jsx
@@ -1,30 +1,55 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Lottie from "lottie-react";
 import loadingAnimation from "@/../public/loading.json";
+import OrientationWarning from "@/app/components/component_warning/warning";
 
 const LoadingScreen = () => {
+  const [isLandscape, setIsLandscape] = useState(
+    typeof window !== "undefined"
+      ? window.innerHeight <= window.innerWidth
+      : false
+  );
+
+  const handleResize = () => {
+    setIsLandscape(window.innerHeight <= window.innerWidth);
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
+      handleResize(); // Verificar la orientación al montar el componente
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-sky-200 z-50">
-      {}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="flex flex-col items-center z-10">
-        <Lottie
-          animationData={loadingAnimation}
-          loop={true}
-          className="w-48 h-48"
-        />
-        <motion.h1
-          className="text-sky-800 text-3xl font-bold mt-4"
-          animate={{ y: [0, -10, 0], opacity: [1, 0.7, 1] }}
-          transition={{ duration: 1.5, repeat: Infinity }}>
-          Cargando...
-        </motion.h1>
-      </motion.div>
+      {!isLandscape ? (
+        <OrientationWarning />
+      ) : (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="flex flex-col items-center z-10">
+          <Lottie
+            animationData={loadingAnimation}
+            loop={true}
+            className="w-48 h-48"
+          />
+          <motion.h1
+            className="text-sky-800 text-3xl font-bold mt-4"
+            animate={{ y: [0, -10, 0], opacity: [1, 0.7, 1] }}
+            transition={{ duration: 1.5, repeat: Infinity }}>
+            Cargando...
+          </motion.h1>
+        </motion.div>
+      )}
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import LoadingScreen from "@/app/components/component_portaf/LoadingScreen";
-import OrientationWarning from "@/app/components/component_warning/warning"; // Importa el componente de advertencia
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -17,10 +16,5 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, [router]);
 
-  return (
-    <>
-      <OrientationWarning /> {/* Incluye el componente de advertencia */}
-      <LoadingScreen />
-    </>
-  );
+  return <LoadingScreen />;
 }
