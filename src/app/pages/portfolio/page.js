ef8a2f7d525f9c57508ccf11cc140787a504ae66
juanCC123 <juanc_villarealo@soy.sena.edu.co>
2024-08-31 15:19:57 -0500

    modificado l

--- src/app/pages/portfolio/page.js
+++ src/app/pages/portfolio/page.js
@@ -7,9 +7,9 @@ import Navbar from "@/app/components/component_portaf/Navbar";
 
 export default function portfolio() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212] text-sm sm:text-base">
       <Navbar />
-      <div className="container mt-12 mx-auto px-6 py-2">
+      <div className="container mt-8 mx-auto px-4 sm:px-6 lg:px-12 py-4 sm:py-6">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
