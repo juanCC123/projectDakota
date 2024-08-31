f5da6abf1c771fa12b8b813a73740acfbcea2888
juanCC123 <juanc_villarealo@soy.sena.edu.co>
2024-08-31 12:06:45 -0500

    modificado

---
+++ src/app/components/component_warning/warning.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OrientationWarning = () => {
+  return (
+    <div className="orientation-warning">
+      <h1>Este sitio solo está disponible en orientación horizontal.</h1>
+      <p>Por favor, rota tu dispositivo para continuar.</p>
+    </div>
+  );
+};
+
+export default OrientationWarning;
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import LoadingScreen from "@/app/components/component_portaf/LoadingScreen";
+import OrientationWarning from "@/app/components/component_warning/warning"; // Importa el componente de advertencia
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -16,5 +17,10 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, [router]);
 
-  return <LoadingScreen />;
+  return (
+    <>
+      <OrientationWarning /> {/* Incluye el componente de advertencia */}
+      <LoadingScreen />
+    </>
+  );
 }
