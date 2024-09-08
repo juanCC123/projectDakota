06362eccc6abba02f7a6ec7f577843cb5f9c4ab2
juanCC123 <juanc_villarealo@soy.sena.edu.co>
2024-09-08 11:31:59 -0500

    modificado 3

--- src/app/pages/micro/page.js
+++
@@ -1,9 +0,0 @@
-import AudioRecorder from "@/app/views/components/component_appMental/Microphone";
-
-export default function MicroP() {
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <AudioRecorder />
-    </div>
-  );
-}
